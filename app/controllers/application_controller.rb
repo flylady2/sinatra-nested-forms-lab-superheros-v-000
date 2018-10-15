026cf56ff16092b07e8216b6c49f0b3f35d44689
Leslie Stevens <flylady@mail.utexas.edu>
2018-10-15 01:30:06 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class App < Sinatra::Base
 
     get '/' do
       erb :'/super_hero'
-      binding.pry
+
     end
 
-    post '/teams' do
+    post '/team' do
 
       @team = Team.new(params[:team])
       params[:team][:heroes].each do |details|
